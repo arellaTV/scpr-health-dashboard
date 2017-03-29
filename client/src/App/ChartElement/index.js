1a95e3d354f2a77fe9efed40e1814a0d7e1d03cc
arellaTV <jeremiah.arella@gmail.com>
2017-03-29 10:49:43 -0700

    Displays with data ingested from google spreadsheets
    Builds friendly arrays for chart.js to use
    
    Charts query independently, methods refactored for modularity
    
    Ingests spreadsheet urls from input and renders new charts
    
    Cleaned up with airBnb Linter
    
    Fix incorrect proptype errorl

--- client/src/App/ChartElement/index.js
+++ client/src/App/ChartElement/index.js
@@ -4,43 +4,72 @@ import Chart from 'chart.js';
 class ChartElement extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      dataArray: [],
+      labelArray: [],
+      type: 'bar',
+    };
+
+    this.getChartData = this.getChartData.bind(this);
+    this.buildChart = this.buildChart.bind(this);
   }
 
   componentDidMount() {
-    const context = document.getElementById(this.props.context);
-    const currentChart = this.props.chart;
-    const myChart = new Chart(context, {
-      type: currentChart.type,
+    const query = `select A, ${this.props.id} order by A`;
+    this.props.googleQuery(query, this.props.dataSourceUrl, this.getChartData);
+  }
+
+  getChartData(response) {
+    const table = response.getDataTable();
+    const tableJSON = JSON.parse(table.toJSON());
+    const dataArray = tableJSON.rows.map(datapoint => datapoint.c[1].v);
+    const labelArray = tableJSON.rows.map(datapoint => datapoint.c[0].f);
+    this.setState({ dataArray, labelArray });
+
+    if (this.state.currentChart) {
+      this.state.currentChart.update();
+    } else {
+      this.buildChart();
+    }
+  }
+
+  buildChart() {
+    this.currentChart = new Chart(this.context, {
+      type: this.state.type,
       data: {
-        labels: currentChart.labels,
+        labels: this.state.labelArray,
         datasets: [{
-            label: currentChart.title,
-            data: currentChart.data,
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255,99,132,1)',
-            borderWidth: 1,
-          }],
+          label: this.props.label,
+          data: this.state.dataArray,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          borderWidth: 1,
+        }],
         options: {
           responsive: true,
-          title: {
-            display: true,
-            text: currentChart.title,
-          }
-        }
-      }
-    })
+        },
+      },
+    });
   }
 
   render() {
     return (
-      <div className={this.props.chart.class}>
-        <canvas id={this.props.context}
-                width={this.props.chart.width}
-                height={this.props.chart.height}>
-        </canvas>
+      <div className="square">
+        <canvas
+          ref={(elem) => { this.context = elem; }}
+          width="400"
+          height="300"
+        />
       </div>
-    )
+    );
   }
 }
+
+ChartElement.propTypes = {
+  id: React.PropTypes.string.isRequired,
+  label: React.PropTypes.string.isRequired,
+  dataSourceUrl: React.PropTypes.string.isRequired,
+  googleQuery: React.PropTypes.func.isRequired,
+};
 
 export default ChartElement;
--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,37 +1,75 @@
 import React from 'react';
+import shortid from 'shortid';
 import ChartElement from './ChartElement';
+import SpreadsheetInput from './SpreadsheetInput';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      charts: []
+      columns: [],
     }
+
+    this.googleQuery = this.googleQuery.bind(this);
+    this.getColumns = this.getColumns.bind(this);
+    this.ingestSpreadsheet = this.ingestSpreadsheet.bind(this);
   }
 
-  componentDidMount() {
-    this.getCharts();
+  ingestSpreadsheet(spreadsheetUrl) {
+    this.googleQuery("SELECT * order by A", spreadsheetUrl, this.getColumns)
   }
 
-  getCharts() {
-    fetch('/charts')
-      .then(response => response.json())
-      .then(charts=> this.setState({ charts }))
+  googleQuery(string, spreadsheetUrl, callback) {
+    const queryString = encodeURIComponent(string);
+    const query = new google.visualization.Query(spreadsheetUrl + queryString);
+    query.send(callback);
+  }
+
+  getColumns(response) {
+    const table = response.getDataTable();
+    const columns = [];
+    for (let i = 1; i < table.getNumberOfColumns(); i += 1) {
+      columns.push({
+        label: table.getColumnLabel(i),
+        id: table.getColumnId(i),
+      });
+    }
+    this.setState({ columns, dataSourceUrl: this.state.dataSourceUrl });
+  }
+
+  ingestSpreadsheet(spreadsheetUrl) {
+    this.googleQuery('SELECT * order by A', spreadsheetUrl, this.getColumns);
+  }
+
+  googleQuery(queryString, spreadsheetUrl, callback) {
+    this.state.dataSourceUrl = spreadsheetUrl;
+    const encodedString = encodeURIComponent(queryString);
+    const suffix = '/gviz/tq?gid=0&headers=1&tq=';
+    const queryUrl = spreadsheetUrl + suffix;
+    const query = new google.visualization.Query(queryUrl + encodedString);
+    query.send(callback);
+  }
+
+  renderColumns() {
+    return this.state.columns.map(column =>
+      <ChartElement
+        key={shortid.generate()}
+        id={column.id}
+        label={column.label}
+        dataSourceUrl={this.state.dataSourceUrl}
+        googleQuery={this.googleQuery}
+      />);
   }
 
   render() {
     return (
       <div>
-        <h1>SCPR Health Metrics Dashboard</h1>
+        <SpreadsheetInput ingestSpreadsheet={this.ingestSpreadsheet} />
         <div className="grid-container">
-        {this.state.charts.map((chart, index) => {
-          return (
-            <ChartElement key={chart.title} context={index} chart={chart}/>
-          )
-        })}
+          {this.renderColumns()}
         </div>
       </div>
-    )
+    );
   }
 }
 
