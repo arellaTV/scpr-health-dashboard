f804234c17d51d590bb3111f091c037ec89767dd
arellaTV <jeremiah.arella@gmail.com>
2017-03-29 15:34:51 -0700

    Builds friendly arrays for chart.js to use

--- client/src/App/ChartElement/index.js
+++ client/src/App/ChartElement/index.js
@@ -7,23 +7,23 @@ class ChartElement extends React.Component {
   }
 
   componentDidMount() {
-    const tableLabels = this.props.tableLabels.slice(1).map(column => column.label);
-    const currentChart = this.props.chart.slice(1);
-    const chartLabel = this.props.chart[0].v;
-    const chartData = currentChart.map(datapoint => {
-      if (datapoint === null) {
-        return null
-      }
-      return datapoint.v;
+    const columnLabel = this.props.columnLabel;
+    const chartData = this.props.chart;
+    const dataLabel = this.props.dataLabel.map(date => {
+      var monthNames = [
+        "January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"
+      ];
+      var month = monthNames[date.getMonth()];
+      return `${month} ${date.getFullYear()}`;
     });
     const context = document.getElementById(this.props.index);
-
     const myChart = new Chart(context, {
-      type: 'line',
+      type: 'bar',
       data: {
-        labels: tableLabels,
+        labels: dataLabel,
         datasets: [{
-            label: chartLabel,
+            label: columnLabel,
             data: chartData,
             backgroundColor: 'rgba(255, 99, 132, 0.2)',
             borderColor: 'rgba(255,99,132,1)',
@@ -33,7 +33,7 @@ class ChartElement extends React.Component {
           responsive: true,
           title: {
             display: true,
-            text: chartLabel,
+            text: columnLabel,
           }
         }
       }
--- client/src/App/index.js
+++ client/src/App/index.js
@@ -5,9 +5,8 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      charts: {
-        rows: []
-      }
+      charts: [],
+      columnLabels: []
     }
   }
 
@@ -23,14 +22,30 @@ class App extends React.Component {
 
   ingestSpreadsheet() {
     const DATA_SOURCE_URL = 'https://docs.google.com/spreadsheets/d/1I9KFzjL6pIraJJScjNyCODlMDSXihpbr9XGzDqWdVuo/gviz/tq?gid=0&headers=1&tq=';
-    const queryString = encodeURIComponent('SELECT *');
+    const queryString = encodeURIComponent('SELECT * order by A');
     const query = new google.visualization.Query(DATA_SOURCE_URL + queryString);
     query.send((response) => {
-      const table = response.getDataTable().toJSON();
-      const charts = JSON.parse(table);
-      console.log(charts);
-      this.setState({ charts });
+      const table = response.getDataTable();
+      this.buildColumnArrays(table);
     });
+  }
+
+  buildColumnArrays(table) {
+    const numberOfColumns = table.getNumberOfColumns();
+    const numberOfRows = table.getNumberOfRows();
+    const chartsArray = [];
+    const columnLabelArray = [];
+    for (let i = 0; i < numberOfColumns; i++) {
+      columnLabelArray.push(table.getColumnLabel(i));
+      chartsArray.push([]);
+      for (let j = 0; j < numberOfRows; j++) {
+        chartsArray[i].push(table.getValue(j, i));
+      }
+    }
+    this.setState({
+      charts: chartsArray,
+      columnLabels: columnLabelArray,
+    })
   }
 
   render() {
@@ -38,12 +53,14 @@ class App extends React.Component {
       <div>
         <h1>SCPR Health Metrics Dashboard</h1>
         <div className="grid-container">
-        {this.state.charts.rows.map((chart, index) => {
+        {this.state.charts.slice(1).map((chart, index) => {
           return (
-            <ChartElement tableLabels={this.state.charts.cols}
-                          chart={chart.c}
-                          index={index}
-                          key={index}/>
+            <ChartElement columnLabel={this.state.columnLabels[index]}
+              chart={chart}
+              index={index}
+              key={index}
+              dataLabel={this.state.charts[0]}
+            />
           )
         })}
         </div>
@@ -53,3 +70,5 @@ class App extends React.Component {
 }
 
 export default App;
+
+
