7301e43b63ed2cf627f78ee1c88323170e38f9b4
arellaTV <jeremiah.arella@gmail.com>
2017-03-30 12:46:22 -0700

    Ingests spreadsheet urls from input and renders new charts
    Cleaned up with airBnb Linter

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,67 +1,71 @@
 import React from 'react';
+import shortid from 'shortid';
 import ChartElement from './ChartElement';
+import SpreadsheetInput from './SpreadsheetInput';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       columns: [],
-      dataSourceUrl: 'https://docs.google.com/spreadsheets/d/1I9KFzjL6pIraJJScjNyCODlMDSXihpbr9XGzDqWdVuo/gviz/tq?gid=0&headers=1&tq=',
-    }
+      dataSourceUrl: 'https://docs.google.com/spreadsheets/d/1I9KFzjL6pIraJJScjNyCODlMDSXihpbr9XGzDqWdVuo/',
+    };
 
     this.googleQuery = this.googleQuery.bind(this);
-    this.getColumns = this.getColumns.bind(this)
+    this.getColumns = this.getColumns.bind(this);
+    this.ingestSpreadsheet = this.ingestSpreadsheet.bind(this);
   }
 
   componentDidMount() {
     this.ingestSpreadsheet(this.state.dataSourceUrl);
   }
 
-  ingestSpreadsheet(spreadsheetUrl) {
-    this.googleQuery("SELECT * order by A", spreadsheetUrl, this.getColumns)
-  }
-
-  googleQuery(string, spreadsheetUrl, callback) {
-    const queryString = encodeURIComponent(string);
-    const query = new google.visualization.Query(spreadsheetUrl + queryString);
-    query.send(callback);
-  }
-
   getColumns(response) {
     const table = response.getDataTable();
     const columns = [];
-    for (let i = 1; i < table.getNumberOfColumns(); i++) {
+    for (let i = 1; i < table.getNumberOfColumns(); i += 1) {
       columns.push({
         label: table.getColumnLabel(i),
-        id: table.getColumnId(i)
+        id: table.getColumnId(i),
       });
     }
-    this.setState({ columns });
+    this.setState({ columns, dataSourceUrl: this.state.dataSourceUrl });
+  }
+
+  ingestSpreadsheet(spreadsheetUrl) {
+    this.googleQuery('SELECT * order by A', spreadsheetUrl, this.getColumns);
+  }
+
+  googleQuery(queryString, spreadsheetUrl, callback) {
+    this.state.dataSourceUrl = spreadsheetUrl;
+    const encodedString = encodeURIComponent(queryString);
+    const suffix = '/gviz/tq?gid=0&headers=1&tq=';
+    const queryUrl = spreadsheetUrl + suffix;
+    const query = new google.visualization.Query(queryUrl + encodedString);
+    query.send(callback);
   }
 
   renderColumns() {
-    return this.state.columns.map((column, index) => {
-      return (
-        <ChartElement key={column.id}
-                      id={column.id}
-                      label={column.label}
-                      dataSourceUrl={this.state.dataSourceUrl}
-                      googleQuery={this.googleQuery}/>
-      )
-    })
+    return this.state.columns.map(column =>
+      <ChartElement
+        key={shortid.generate()}
+        id={column.id}
+        label={column.label}
+        dataSourceUrl={this.state.dataSourceUrl}
+        googleQuery={this.googleQuery}
+      />);
   }
 
   render() {
     return (
       <div>
+        <SpreadsheetInput ingestSpreadsheet={this.ingestSpreadsheet} />
         <div className="grid-container">
-        {this.renderColumns()}
+          {this.renderColumns()}
         </div>
       </div>
-    )
+    );
   }
 }
 
 export default App;
-
-
